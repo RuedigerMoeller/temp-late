49bf70b8ab2f78ff78f212cab11143f8b2874b67
RuedigerMoeller <moru0011@gmail.com>
2014-05-26 20:01:12 +0200

    .

---
+++ src/main/java/de/ruedigermoeller/templatesample/TemplateSample.java
@@ -0,0 +1,29 @@
+package de.ruedigermoeller.templatesample;
+
+import de.ruedigermoeller.template.TemplateExecutor;
+
+/**
+ * Created by ruedi on 26.05.14.
+ *
+ * Run from project root for correct pathes
+ *
+ */
+public class TemplateSample {
+
+    public static class TemplateContext {
+
+        public String clazzName;
+        public String getters[] = { "Test", "Other", "Example" };
+
+        public TemplateContext(String clazzName) {
+            this.clazzName = clazzName;
+        }
+
+    }
+
+    public static void main( String arg[] ) {
+
+        TemplateExecutor.Run("./src/main/resources/sample.jsp", new TemplateContext("GeneratedClass"));
+
+    }
+}
